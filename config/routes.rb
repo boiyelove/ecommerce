1c15a11d8ee21f081fc2b8002ce60a8cab689a47
boiyelove <daahrmmieboiye@gmail.com>
2020-03-27 05:01:49 +0100

    set product index as root url

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   resources :order_items
   resources :products
   resources :users
+
+  root :to => 'products#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
