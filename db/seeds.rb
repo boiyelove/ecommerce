b80ae3abaf47683eba3a69206046993c6804f8c5
boiyelove <daahrmmieboiye@gmail.com>
2020-04-01 11:06:32 +0100

    added users and payment info seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,5 +20,27 @@ data.each do |row|
 	Product.create!(row.to_hash)
 end
 
+# create user
+data = SmarterCSV.process(Rails.root.join("db", "customers.csv"), {headers_in_file: true, user_provided_headers: %i[customer_name cardnumber cvv expiry city]})
+
+data.each do |row|
+	name = row[:customer_name].split(" ")
+	email =  name.first + name.last + '@' + Faker::Internet.domain_name
+	user = User.new(firstname: name.first, lastname: name.last, email: email, password: "password1234", password_confirmation: "password1234")
+	user.save!
+	payinfo = PaymentInfo.create!(card_number: row[:cardnumber], card_pin: row[:cvv], card_exp: row[:expiry], address: Faker::Address.street_address, state: Faker::Address.state, country: Faker::Address.country_code, user: user)
+	payinfo.save!
+end
+
+# end
+# 50.times do |i|
+# 	user = User.new(firstname: Faker::Name.first_name, lastname: Faker::Name.last_name, email: Faker::Internet.emal, password: "password1234", password_confirmation: "password1234")
+# 	user.save!
+# end
+
+#create order items 
+# create payment info
+# create order
+
 
 
