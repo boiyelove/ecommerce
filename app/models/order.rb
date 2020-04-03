828d0f73cafed9a6eec26b2175a04b9e957318bb
boiyelove <daahrmmieboiye@gmail.com>
2020-04-03 13:39:36 +0100

    implemented admin single order view

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,8 @@
 class Order < ApplicationRecord
   belongs_to :user
   has_many :order_items, dependent: :destroy
-  
+  has_one :payment_info
+
 	def sub_total
 		sum = 0
 		self.order_items.each do|order_item|
@@ -16,5 +17,10 @@ class Order < ApplicationRecord
 			sum += order_item.quantity
 		end
 		return sum
+	end
+
+	def shipping_address
+		payinfo = PaymentInfo.find_by_id(self.payment_info_id)
+		address = payinfo.address + ', ' + payinfo.state + ', ' + payinfo.country
 	end
 end
