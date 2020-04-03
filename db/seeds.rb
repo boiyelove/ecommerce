f1a42e21676753ad94792b028a54979b768318d9
boiyelove <daahrmmieboiye@gmail.com>
2020-04-03 14:53:42 +0100

    modified admin dashboard graphs

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,27 +23,48 @@ def rand_in_range(from, to)
   rand * (to - from) + from
 end
 
-# create product
-puts "Seeding products"
-data = SmarterCSV.process(Rails.root.join("db", "productseed.csv"), {headers_in_file: true, user_provided_headers: %i[title description price]})
-data.each do |row|
-	Product.create!(row.to_hash)
+# #create product
+# puts "Seeding products"
+# data = SmarterCSV.process(Rails.root.join("db", "productseed.csv"), {headers_in_file: true, user_provided_headers: %i[title description price]})
+# data.each do |row|
+# 	Product.create!(row.to_hash)
+# end
+
+
+# # create user
+# puts "Seeding Users and payment information"
+# data = SmarterCSV.process(Rails.root.join("db", "customers.csv"), {headers_in_file: true, user_provided_headers: %i[customer_name cardnumber cvv expiry city]})
+
+# data.each do |row|
+# 	name = row[:customer_name].split(" ")
+# 	created_at = rand_time(90.days.ago)
+# 	email =  name.first + name.last + '@' + Faker::Internet.domain_name
+# 	user = User.new(firstname: name.first, lastname: name.last, email: email.downcase, password: "password1234", password_confirmation: "password1234", created_at: created_at)
+# 	user.save!
+# 	payinfo = PaymentInfo.create!(card_number: row[:cardnumber], card_pin: row[:cvv], card_exp: row[:expiry], address: Faker::Address.street_address, state: Faker::Address.state, country: Faker::Address.country_code, user: user, created_at: created_at)
+# 	payinfo.save!
+# end
+
+
+puts "Seeding Orders"
+users = User.all
+users.each do |user|
+	order = Order.new
+		10.times do |i|
+			order_item = OrderItem.new
+			product = Product.find(rand_in_range(1, Product.all.count))
+			order_item.product = product
+			order_item.quantity = rand_in_range(1, 10)
+			order_item.save!
+			order.order_items << order_item
+		end
+	order.user = user
+	payinfo = PaymentInfo.find_by(user_id: user.id)
+	order.payment_info_id = payinfo.id
+	order.created_at = rand_time(60.days.ago)
+	order.save!
 end
 
-
-# create user
-puts "Seeding Users and payment information"
-data = SmarterCSV.process(Rails.root.join("db", "customers.csv"), {headers_in_file: true, user_provided_headers: %i[customer_name cardnumber cvv expiry city]})
-
-data.each do |row|
-	name = row[:customer_name].split(" ")
-	created_at = rand_time(90.days.ago)
-	email =  name.first + name.last + '@' + Faker::Internet.domain_name
-	user = User.new(firstname: name.first, lastname: name.last, email: email.downcase, password: "password1234", password_confirmation: "password1234", created_at: created_at)
-	user.save!
-	payinfo = PaymentInfo.create!(card_number: row[:cardnumber], card_pin: row[:cvv], card_exp: row[:expiry], address: Faker::Address.street_address, state: Faker::Address.state, country: Faker::Address.country_code, user: user, created_at: created_at)
-	payinfo.save!
-end
 puts "done!"
 # end
 # 50.times do |i|
